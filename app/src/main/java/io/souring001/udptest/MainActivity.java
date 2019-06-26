ddfeefd4794c50c070fe963ff34750ea436fd9a8
aso <titechei@gmail.com>
2019-06-27 01:10:23 +0900

    get Roll Pitch Yaw

--- app/src/main/java/io/souring001/udptest/MainActivity.java
+++ app/src/main/java/io/souring001/udptest/MainActivity.java
@@ -1,21 +1,36 @@
 package io.souring001.udptest;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private final static String TAG = MainActivity.class.getSimpleName();
+    final static double RAD2DEG = 180/Math.PI;
+
+    SensorManager sensorManager;
+
+    float[] rotationMatrix = new float[9];
+    float[] gravity = new float[3];
+    float[] geomagnetic = new float[3];
+    float[] attitude = new float[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initSensor();
 
         final int port = Integer.parseInt(getString(R.string.port));
         final String address = getString(R.string.address);
@@ -25,7 +40,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 while(true){
+
                     String msg = "Hello World! I'm Android";
+
+                    Log.d(TAG, Integer.toString((int)(attitude[0] * RAD2DEG)));
+                    Log.d(TAG, Integer.toString((int)(attitude[1] * RAD2DEG)));
+                    Log.d(TAG, Integer.toString((int)(attitude[2] * RAD2DEG)));
+
                     try {
                         InetAddress IPAddress = InetAddress.getByName(address);
                         byte[] strByte = msg.getBytes("ASCII");
@@ -50,5 +71,58 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }).start();
+    }
+
+    protected void initSensor(){
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        if (sensorManager == null) {
+            Toast.makeText(this, R.string.toast_no_sensor_manager, Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+    }
+
+    public void onResume(){
+        super.onResume();
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    public void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        switch(event.sensor.getType()){
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                geomagnetic = event.values.clone();
+                break;
+            case Sensor.TYPE_ACCELEROMETER:
+                gravity = event.values.clone();
+                break;
+        }
+
+        if(geomagnetic != null && gravity != null){
+
+            SensorManager.getRotationMatrix(
+                    rotationMatrix, null,
+                    gravity, geomagnetic);
+
+            SensorManager.getOrientation(
+                    rotationMatrix,
+                    attitude);
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.d(TAG, "onAccuracyChanged: accuracy=" + accuracy);
     }
 }
